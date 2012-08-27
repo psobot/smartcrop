b35bfd1c28797741de7826718e7251dc1bf58337
Peter Sobot <psobot@gmail.com>
2012-08-26 21:53:39 -0700

    Initial Commit

---
+++ SmartCrop/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SmartCrop
+//
+//  Created by Peter Sobot on 8/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
